0683099e57c8aa0087b0c3c3d9b9b631750a5fdf
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2024-03-03 23:26:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,7 +106,7 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://moviesuniverse-704fef876ee6.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+URL = "https://moviesuniverse-704fef876ee6.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://moviesuniverse-704fef876ee6.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
