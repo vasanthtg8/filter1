5ac04d0873f66a24b9bc32135950a2f7904c1c13
Ironman616 <150930956+Ironman616@users.noreply.github.com>
2024-03-07 01:04:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,7 +97,7 @@ SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "seas
 
 # Online Stream and Download
 NO_PORT = bool(environ.get('NO_PORT', False))
-APP_NAME = None
+APP_NAME = moviesuniverse1
 if 'DYNO' in environ:
     ON_HEROKU = True
     APP_NAME = environ.get('APP_NAME')
